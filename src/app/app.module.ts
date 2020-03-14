414627dedceb8e79857e57865bf4c251e05dcc78
Stefanie S <sts@s-blu.de>
2020-03-14 20:00:47 +0100

    initialize quill editor, add demo template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuillComponent } from './quill/quill.component';
+import { QuillModule } from 'ngx-quill';
+import { SimplebarAngularModule } from 'simplebar-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuillComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SimplebarAngularModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
