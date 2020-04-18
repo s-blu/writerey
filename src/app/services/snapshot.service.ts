1a752fda03ef130bda8efa406ebed8b2fab02bfd
Stefanie S <sts@s-blu.de>
2020-04-18 12:18:12 +0200

    feat(#26): add interval obs for automated commits

--- src/app/services/snapshot.service.ts
+++ src/app/services/snapshot.service.ts
@@ -1,15 +1,22 @@
+import { TranslocoService } from '@ngneat/transloco';
 import { SnapshotStore } from './../stores/snapshot.store';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap, take } from 'rxjs/operators';
 
 import { ApiService } from './api.service';
+import { interval } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SnapshotService {
-  constructor(private httpClient: HttpClient, private api: ApiService, private snapshotStore: SnapshotStore) {}
+  constructor(
+    private httpClient: HttpClient,
+    private api: ApiService,
+    private snapshotStore: SnapshotStore,
+    private transloco: TranslocoService
+  ) {}
 
   createSnapshot(msg) {
     const formdata = new FormData();
@@ -47,5 +54,14 @@ export class SnapshotService {
 
   init() {
     this.getSnapshotInfo().pipe(take(1)).subscribe();
+
+    // fixme set interval to 15 - 30 minutes
+    interval(1000 * 60 * 2).subscribe(() => {
+      console.log('creating automated snapshot...');
+      const msg = this.transloco.translate('git.message.automateCommit', { date: new Date().toISOString() });
+      this.createSnapshot(msg)
+        .pipe(take(1))
+        .subscribe(() => console.log('successfully created automated snapshot'));
+    });
   }
 }
