2d5ae3c57ca503cda0752dbcc2d3fdba0152d3c0
Stefanie S <sts@s-blu.de>
2020-06-07 11:28:26 +0200

    fix(#65): do not create file/dir if name is in use

--- src/app/components/_documents/createNewDirOrFile/createNewDirOrFile.component.ts
+++ src/app/components/_documents/createNewDirOrFile/createNewDirOrFile.component.ts
@@ -1,10 +1,15 @@
+import { StripFileEndingPipe } from './../../../pipes/stripFileEnding.pipe';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DirectoryStore } from './../../../stores/directory.store';
 import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, merge, forkJoin, combineLatest, zip, of } from 'rxjs';
 import { CreateNewItemDialogComponent } from '../../createNewItemDialog/createNewItemDialog.component';
 import { DocumentService } from 'src/app/services/document.service';
 import { DirectoryService } from 'src/app/services/directory.service';
 import { DocumentStore } from 'src/app/stores/document.store';
+import { mergeAll, mergeMap, take, flatMap } from 'rxjs/operators';
+import { translate } from '@ngneat/transloco';
 
 @Component({
   selector: 'wy-create-new-dir-or-file',
@@ -24,8 +29,11 @@ export class CreateNewDirOrFileComponent implements OnInit, OnDestroy {
   constructor(
     public dialog: MatDialog,
     private documentService: DocumentService,
+    private documentStore: DocumentStore,
     private directoryService: DirectoryService,
-    private documentStore: DocumentStore
+    private directoryStore: DirectoryStore,
+    private snackBar: MatSnackBar,
+    private stripFileEndingPipe: StripFileEndingPipe
   ) {}
 
   ngOnInit() {}
@@ -34,23 +42,60 @@ export class CreateNewDirOrFileComponent implements OnInit, OnDestroy {
     const dialogRef = this.dialog.open(CreateNewItemDialogComponent, {
       data: { dirPath: this.path, typeOfDialog: this.type },
     });
+    zip(dialogRef.afterClosed(), this.directoryStore.tree$)
+      .pipe(
+        take(1),
+        flatMap(([name, tree]) => {
+          if (!name) return;
+          console.log('zipped', name, tree, this.path);
+          const containingDir = this.getDirSubTree(tree);
+          console.log('containingDir', containingDir);
+          let createObservable;
 
-    this.subscription.add(
-      dialogRef.afterClosed().subscribe(name => {
-        if (!name) return;
-        let createObservable;
-        if (this.type === 'file') createObservable = this.documentService.createDocument(this.path, name);
-        if (this.type === 'dir') createObservable = this.directoryService.createDirectory(this.path, name);
-
-        this.subscription.add(
-          createObservable.subscribe((res: any) => {
-            this.directoryService.getTree().subscribe(_ => {
-              this.itemCreated.emit(res);
-              if (this.type === 'file') this.documentStore.setFileInfo({ name: res.name, path: res.path });
+          if (this.type === 'file') {
+            const existing = containingDir.files.find(file => {
+              return this.stripFileEndingPipe.transform(file.name).toLowerCase() === name.toLowerCase();
             });
-          })
-        );
-      })
-    );
+            if (!existing) createObservable = this.documentService.createDocument(this.path, name);
+          } else if (this.type === 'dir') {
+            const existing = containingDir.dirs.find(dir => dir.name.toLowerCase() === name.toLowerCase());
+            if (!existing) createObservable = this.directoryService.createDirectory(this.path, name);
+          }
+
+          if (!createObservable) {
+            const snackBarMsg = translate('createDirOrFile.alreadyInUse');
+            this.snackBar.open(snackBarMsg, null, {
+              duration: 5000,
+              horizontalPosition: 'right',
+            });
+            return of(null);
+          }
+
+          return createObservable;
+        })
+      )
+      .subscribe((res: any) => {
+        if (!res) return;
+        this.directoryService.getTree().subscribe(_ => {
+          this.itemCreated.emit(res);
+          if (this.type === 'file') this.documentStore.setFileInfo({ name: res.name, path: res.path });
+        });
+      });
+  }
+
+  private getDirSubTree(tree) {
+    const pathParts = this.path.split('/');
+    pathParts.shift();
+    console.log(pathParts);
+    if (pathParts.length === 0) return tree;
+    let subTree = tree;
+    for (const pathPart of pathParts) {
+      if (!subTree) {
+        console.error('Could not find subtree to check for name existence', pathPart, tree);
+        return tree;
+      }
+      subTree = subTree.dirs.find(dir => dir.name === pathPart);
+    }
+    return subTree;
   }
 }
