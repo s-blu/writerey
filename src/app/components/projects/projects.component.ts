3f4722d13f0e5089f16ac7e07f5209742f6e152d
Stefanie S <sts@s-blu.de>
2020-06-07 10:02:17 +0200

    fix(#65): check if project name is already in use

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { CreateNewItemDialogComponent } from '../createNewItemDialog/createNewItemDialog.component';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { translate } from '@ngneat/transloco';
 
 @Component({
   selector: 'wy-projects',
@@ -18,7 +20,7 @@ export class ProjectsComponent implements OnInit {
 
   private subscription = new Subscription();
 
-  constructor(private dialog: MatDialog, private directoryService: DirectoryService) {}
+  constructor(private dialog: MatDialog, private directoryService: DirectoryService, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.fetchProjects();
@@ -38,6 +40,15 @@ export class ProjectsComponent implements OnInit {
       dialogRef.afterClosed().subscribe(name => {
         if (!name) return;
 
+        if (this.isNameAlreadyInUse(name)) {
+          const snackBarMsg = translate('projects.nameAlreadyInUse');
+          this.snackBar.open(snackBarMsg, null, {
+            duration: 5000,
+            horizontalPosition: 'right',
+          });
+          return;
+        }
+
         this.subscription.add(
           this.directoryService.createDirectory('/', name).subscribe((res: any) => {
             this.fetchProjects();
@@ -46,6 +57,11 @@ export class ProjectsComponent implements OnInit {
         );
       })
     );
+  }
+
+  private isNameAlreadyInUse(name): boolean {
+    if (!name || !this.projects) return false;
+    return this.projects.some(p => p.name.toLowerCase() === name.toLowerCase());
   }
 
   private fetchProjects() {
