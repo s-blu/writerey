abc2978b0f737d263947f8b2b54e6e3fa8e53379
Stefanie S <sts@s-blu.de>
2020-06-20 15:55:52 +0200

    feat(init): show alert if server is unreachable, shorten retry time

--- src/app/services/wy-init.service.ts
+++ src/app/services/wy-init.service.ts
@@ -1,3 +1,4 @@
+import { catchError } from 'rxjs/operators';
 // Copyright (c) 2020 s-blu
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
@@ -34,13 +35,20 @@ export class WyInitService {
 
   init() {
     this.addWritereyIconToMatIcon();
-    this.waitForServerStartup().subscribe(_ => {
-      console.log('Server is ready, proceeding with init of frontend ...');
-      this.labelService.init();
-      this.snapshotService.init();
-      this.documentService.init();
-      this.directoryService.init();
-    });
+    this.waitForServerStartup()
+      .pipe(
+        catchError(err => {
+          window.alert('Could not start up server. Please restart the application. Error: ' + err?.message);
+          return throwError(err);
+        })
+      )
+      .subscribe(_ => {
+        console.log('Server is ready, proceeding with init of frontend ...');
+        this.labelService.init();
+        this.snapshotService.init();
+        this.documentService.init();
+        this.directoryService.init();
+      });
   }
 
   private addWritereyIconToMatIcon() {
@@ -51,14 +59,15 @@ export class WyInitService {
   }
 
   private waitForServerStartup() {
-    const maxRetries = 4;
-    let retries = maxRetries;
+    let retries = 8;
 
     return this.http.get(this.api.getServerReadyRoute()).pipe(
       retryWhen(response =>
         response.pipe(
-          tap(_ => console.warn(`Ping was not successfull, server could not be reached. Retry ${retries} of ${maxRetries}.`)),
-          delay(1000),
+          tap(_ =>
+            console.warn(`Ping was not successfull, server could not be reached. Retrying ${retries} more times...`)
+          ),
+          delay(400),
           mergeMap(err => (retries-- > 0 ? of(err) : throwError(err)))
         )
       )
