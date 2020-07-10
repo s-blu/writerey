a36b28dc5831ea0efb6d888df4a31a90bd80cee6
Stefanie S <sts@s-blu.de>
2020-07-10 15:49:15 +0200

    fix(lastModified): prevent animation on document switch

--- src/app/components/lastModified/lastModified.component.ts
+++ src/app/components/lastModified/lastModified.component.ts
@@ -1,16 +1,15 @@
 // Copyright (c) 2020 s-blu
-// 
+//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import { DocumentStore } from './../../stores/document.store';
-import { Subscription } from 'rxjs';
+import { Subscription, combineLatest } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SnapshotStore } from 'src/app/stores/snapshot.store';
-import {
-  wobbleAnimation,
-} from 'angular-animations';
+import { wobbleAnimation } from 'angular-animations';
+import { tap, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'wy-last-modified',
@@ -23,16 +22,31 @@ export class LastModifiedComponent implements OnInit, OnDestroy {
   lastSave: Date;
   saved = false;
 
+  private fileInfo;
+  private previousLastSave;
   private subscription = new Subscription();
   constructor(private snapshotStore: SnapshotStore, private documentStore: DocumentStore) {}
 
   ngOnInit() {
     this.subscription.add(this.snapshotStore.snapshotDate$.subscribe(res => (this.lastSnapshot = res)));
     this.subscription.add(
-      this.documentStore.lastSaved$.subscribe(res => {
-        this.lastSave = res;
-        this.saved = !this.saved;
-      })
+      this.documentStore.lastSaved$
+        .pipe(
+          tap(res => {
+            this.previousLastSave = this.lastSave;
+            this.lastSave = res;
+          }),
+          flatMap(_ => this.documentStore.fileInfo$)
+        )
+        .subscribe(fileInfo => {
+          if (this.previousLastSave === this.lastSave) return;
+          
+          if (this.fileInfo !== fileInfo) {
+            this.fileInfo = fileInfo;
+          } else if (this.previousLastSave) {
+            this.saved = !this.saved;
+          }
+        })
     );
   }
 
