c1e32055f6a787ddd1b640e09f53ab3563726d08
Stefanie S <sts@s-blu.de>
2020-07-26 17:22:28 +0200

    chore(#120): provide pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {StripFileEndingPipe} from './pipes/stripFileEnding.pipe';
+/**
+ * This module
+ * - should consist entirely of declarations, most of them exported.
+ * - may re-export other widget modules, such as CommonModule, FormsModule, and NgModules with the UI controls that you use most widely.
+ * - should not have providers for reasons explained previously. Nor should any of its imported or re-exported modules have providers.
+ * https://angular.io/guide/ngmodule-faq#sharedmodule
+ */
 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StripFileEndingPipe } from './pipes/stripFileEnding.pipe';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: [StripFileEndingPipe],
   exports: [StripFileEndingPipe],
-  providers: [
-    StripFileEndingPipe]
+  // FIXME need to provide that to be able to use it in services. Clashes with Best Practices from Angular. Help.
+  providers: [StripFileEndingPipe],
 })
-export class SharedModule {
-}
+export class SharedModule {}
