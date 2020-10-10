f50a4e57a1b900588efd522c75e48effa7a800a2
Stefanie S <git@s-blu.de>
2020-10-10 19:09:14 +0200

    fix(#78): remove karma dependencies

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,6 @@ const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
-  testPathIgnorePatterns: ["<rootDir>/src/test.ts", "out"],
+  testPathIgnorePatterns: ["out"],
   moduleNameMapper,
 };
