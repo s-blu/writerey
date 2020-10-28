8ab89a155ee96a437e42c0ca85f1831af47c8032
Stefanie S <git@s-blu.de>
2020-10-28 17:24:10 +0100

    test(#174): get createNewLink spec running

--- src/app/noteItems/components/upsertNote/upsertNote.component.spec.ts
+++ src/app/noteItems/components/upsertNote/upsertNote.component.spec.ts
@@ -18,6 +18,7 @@ import { UpsertNoteComponent } from './upsertNote.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ControlValueAccessor } from '@angular/forms';
 import { NOTE_DRAFT_KEY } from './upsertNote.component';
+import { MockCkeditorComponent } from '@writerey/shared/test/ckeditor.component.mock';
 
 const EMPTY_TEXT = ' \n';
 
@@ -26,37 +27,6 @@ class MockLocalStore {
   setItem(key: string, value: string) {}
   removeItem(key: string) {}
   clear() {}
-}
-
-@Component({
-  // tslint:disable-next-line: component-selector
-  selector: 'ckeditor',
-  template: '<div>mock ckeditor</div>',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => MockCkeditorComponent),
-      multi: true,
-    },
-  ],
-})
-class MockCkeditorComponent implements ControlValueAccessor {
-  @Input() editor;
-  @Input() config;
-  @Output() ready = new EventEmitter();
-  // tslint:disable-next-line: no-output-native
-  @Output() change = new EventEmitter();
-
-  /**
-   * MockComponent needs to implement ControlValueAccessor interface if used in a formBuilder
-   * meaning if given a [formControl] or [(ngModel)] binding
-   */
-  @Input() disabled = false;
-
-  writeValue(obj: any): void {}
-  registerOnChange(fn: any): void {}
-  registerOnTouched(fn: any): void {}
-  setDisabledState?(isDisabled: boolean): void {}
 }
 
 describe('CreateNewNoteComponent', () => {
