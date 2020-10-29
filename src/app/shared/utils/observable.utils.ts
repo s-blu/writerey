2049789aca5415acab0b5980d9dc7fc256c44eff
Stefanie S <git@s-blu.de>
2020-10-29 20:43:21 +0100

    Deployed 2b40c9d with MkDocs version: 1.1.2

--- src/app/shared/utils/observable.utils.ts
+++
@@ -1,8 +0,0 @@
-import { from, interval, Observable, zip } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-export function delayValues(values: any, delay: number = 100) {
-  if (!(values instanceof Observable)) values = from(values);
-
-  return zip(values, interval(delay)).pipe(map(([val, _]) => val));
-}
