670d287dba73e7b65798003029770bacdd5e6ea7
Stefanie S <git@s-blu.de>
2020-11-04 09:24:28 +0100

    fix(#185): change piplock require to 3 try to use pip3 if pip is failing

--- server/initialize_env.py
+++ server/initialize_env.py
@@ -9,7 +9,7 @@ import os
 from pathlib import Path
 
 from writerey_config import basePath
-
+# TODO: is this package named pip under ubuntu, too?
 from pip._internal.utils.misc import get_installed_distributions
 from logger import Logger
 
@@ -17,12 +17,18 @@ installed_packages = get_installed_distributions()
 flat_installed_packages = [package.project_name for package in installed_packages]
 
 def initialize_env(): 
+  pip_cmd = 'pip'
+  try:
+    subprocess.run([pip_cmd, '--version'], shell=True, check=True, text=True)
+  except:
+    pip_cmd = 'pip3'
+  
   log = Logger('initialize_app')
   required_packages = ['Flask', 'Flask-RESTful', 'waitress']
   log.logDebug('installed packages ...', flat_installed_packages)
   for package in required_packages:
     if package not in flat_installed_packages:
-      subprocess.run(["pip", "install", package], shell=True, check=True, text=True)
+      subprocess.run([pip_cmd, "install", package], shell=True, check=True, text=True)
     else:
       log.logDebug('Package is installed, do nothing... ', package)
       
