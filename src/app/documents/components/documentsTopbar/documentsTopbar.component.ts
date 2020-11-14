625051b5933c431b4127659098a6933583d6b2c9
Stefanie S <git@s-blu.de>
2020-11-14 12:57:41 +0100

    refactor(#164): remove unused imports

--- src/app/documents/components/documentsTopbar/documentsTopbar.component.ts
+++ src/app/documents/components/documentsTopbar/documentsTopbar.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'wy-documentsTopbar',
+  selector: 'wy-documents-topbar',
   templateUrl: './documentsTopbar.component.html',
   styleUrls: ['./documentsTopbar.component.scss'],
 })
